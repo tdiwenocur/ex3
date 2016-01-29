e437217169a104c25a7171a7d9417c39b4098381
tdiwenocur <diwenocur@ucdavis.edu>
2016-01-28 18:34:30 -0800

    Initial commit

---
+++ time.js
@@ -0,0 +1,141 @@
+$(document).ready(function() {
+  console.log('ready');
+
+    //hide all events
+    $('#calendar p').hide();
+
+    //tooltip for 1:00 (hover takes two functions)
+	// first function
+    $('#one h2').hover(function() { 
+	//turn on the tool tip
+     $('#one p').show();
+	 // second function
+ }, function() { 
+	
+      $('#one p').hide();
+
+    });
+	   //tooltip for two (hover takes two functions)
+	// first function
+    $('#two h2').hover(function() { 
+	//turn on the tool tip
+     $('#two p').show();
+	 // second function
+ }, function() { 
+	
+      $('#two p').hide();
+
+    });
+
+   //tooltip for eat (hover takes two functions)
+	// first function
+    $('#three h2').hover(function() { 
+	//turn on the tool tip
+     $('#three p').show();
+	 // second function
+ }, function() { 
+	
+      $('#three p').hide();
+
+    });
+  
+ 
+
+   //tooltip for four (hover takes two functions)
+	// first function
+    $('#four h2').hover(function() { 
+	//turn on the tool tip
+     $('#four p').show();
+	 // second function
+ }, function() { 
+	
+      $('#four p').hide();
+
+    });
+	
+	   //tooltip for five (hover takes two functions)
+	// first function
+    $('#five h2').hover(function() { 
+	//turn on the tool tip
+     $('#five p').show();
+	 // second function
+ }, function() { 
+	
+      $('#five p').hide();
+
+    });
+	
+   //tooltip for six (hover takes two functions)
+	// first function
+    $('#six h2').hover(function() { 
+	//turn on the tool tip
+     $('#six p').show();
+	 // second function
+ }, function() { 
+	
+      $('#six p').hide();
+
+    });
+
+   //tooltip for yellow (hover takes two functions)
+	// first function
+    $('#yellowpic').hover(function() { 
+	//turn on the tool tip
+     $('.yellow').show();
+	 // second function
+ }, function() { 
+	
+      $('.yellow').hide();
+
+    });	
+	
+	  //tooltip for red (hover takes two functions)
+	// first function
+    $('#redpic').hover(function() { 
+	//turn on the tool tip
+     $('.red').show();
+	 // second function
+ }, function() { 
+	
+      $('.red').hide();
+
+    });	
+	
+  //tooltip for pink (hover takes two functions)
+	// first function
+    $('#pinkpic').hover(function() { 
+	//turn on the tool tip
+     $('.pink').show();
+	 // second function
+ }, function() { 
+	
+      $('.pink').hide();
+
+    });	
+  //tooltip for blue (hover takes two functions)
+	// first function
+    $('#bluepic').hover(function() { 
+	//turn on the tool tip
+     $('.blue').show();
+	 // second function
+ }, function() { 
+	
+      $('.blue').hide();
+
+    });	
+	
+	//tooltip for all the things (hover takes two functions)
+	// first function
+    $('h1').hover(function() { 
+	//turn on the tool tip
+     $('#calendar p').show();
+	 // second function
+ }, function() { 
+	
+      $('#calendar p').hide();
+
+    });	
+
+//});
+
+});
